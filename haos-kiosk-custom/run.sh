0bbfc72fc0f14c5b5450b4cbafbf883613345d5a
atila593 <108370757+atila593@users.noreply.github.com>
2025-12-10 11:14:05 +0100

    Update run.sh

--- haos-kiosk-custom/run.sh
+++ haos-kiosk-custom/run.sh
@@ -154,8 +154,7 @@ else
     cat > /etc/X11/xorg.conf << 'EOF'
 Section "Device"
     Identifier "Card0"
-    Driver "intel"
-    Option "AccelMethod" "sna"
+    Driver "modesetting"
 EndSection
 
 Section "Screen"
